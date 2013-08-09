4c8f05cc8412bfd4347444f18fa6dedc70bfd792
Pavel Lepin <pbl64k@gmail.com>
2013-08-09 13:50:19 +0300

    Interesting.

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 
 from client import Client
 
+from bv_parser import parse
+from bv import evl
+
 import json
 
 cl = Client()
 
-st, msg, resp = cl.invoke('status', '')
+#st, msg, resp = cl.invoke('status', '')
+#
+#print 'Status:', st
+#print 'Reason:', msg
+#print 'Response:', json.dumps(resp)
+#
+#st, msg, resp = cl.invoke('train', '')
+#
+#print 'Status:', st
+#print 'Reason:', msg
+#print 'Response:', resp
 
-print 'Status:', st
-print 'Reason:', msg
-print 'Response:', json.dumps(resp)
+p = parse(u'(lambda (x_15083) (fold x_15083 0 (lambda (x_15083 x_15084) (if0 (shr4 (shr1 (or 1 x_15083))) x_15083 x_15084))))'.encode('latin1'))
+print evl(p, 0x0300001000000000)
 
