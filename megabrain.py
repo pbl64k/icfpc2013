61dd4ce08b41347bf1bc52b8487cdc09f1b64b54
Pavel Lepin <pbl64k@gmail.com>
2013-08-09 14:41:50 +0300

    Hmmm. This calls for some aggressive caching.

--- megabrain.py
+++ megabrain.py
@@ -3,6 +3,27 @@ from bv_parser import parse
 from bv_meta import *
 from bv import evl
 
+def process(resp):
+    for x in resp:
+        solved = 'solved' in x and x['solved']
+        print 'size:', x['size'], 'ops:', x['operators'], 'id:', x['id'], ('SOLVED!' if solved else 'unsolved.')
+        if solved:
+            continue
+        if x['size'] == 3 and len(x['operators']) == 1:
+            print
+            print 'Solving...'
+            code = solve_3(x['id'], x['size'], x['operators'])
+            print code
+            p = parse(code)
+            print p
+            print sz(p)
+            print ops(p)
+            print
+            cl.guess(x['id'], code)
+            break
+        if x['size'] > 4:
+            break
+
 def solve_3(pid, size, opers):
     assert size == 3
     assert len(opers) == 1
--- test.py
+++ test.py
@@ -16,25 +16,7 @@ cl = Client()
 
 resp = cl.myproblems()
 
-for x in resp:
-    solved = 'solved' in x and x['solved']
-    print 'size:', x['size'], 'ops:', x['operators'], 'id:', x['id'], ('SOLVED!' if solved else 'unsolved.')
-    if solved:
-        continue
-    if x['size'] == 3 and len(x['operators']) == 1:
-        print
-        print 'Solving...'
-        code = solve_3(x['id'], x['size'], x['operators'])
-        print code
-        p = parse(code)
-        print p
-        print sz(p)
-        print ops(p)
-        print
-        cl.guess(x['id'], code)
-        break
-    if x['size'] > 4:
-        break
+process(resp)
 
 cl.print_status()
 
