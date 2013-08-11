a114041d74ce68b11cc932f0c578c1d47074f277
Pavel Lepin <pbl64k@gmail.com>
2013-08-11 11:24:57 +0300

    Hmm.

--- model.py
+++ model.py
@@ -3,9 +3,62 @@ from z3 import *
 
 init('./Z3/z3-4.3.2.30df2837fbff-x86-ubuntu-12.04/bin/libz3.so')
 
-x = Int('x')
-y = Int('y')
-solve(x > 2, y < 10, x + 2*y == 7)
+#x = Int('x')
+#y = Int('y')
+#solve(x > 2, y < 10, x + 2*y == 7)
+#
+#x = BitVec('x', 8)
+#solve(UGT(x, BitVecVal(0, 8)), x + x == BitVecVal(0, 8))
+
+def solve_model(sz, operators, vals):
+    s = Solver()
+
+    zero = BitVecVal(0, 64)
+    one = BitVecVal(1, 64)
+
+    steps = sz - 2
+
+    for x in vals:
+        fx = vals[x]
+
+        m_x = BitVecVal(x, 64)
+        m_fx = BitVecVal(fx, 64)
+
+        ops = []
+
+        for step in range(steps):
+            ops.append({})
+            for op in operators:
+                ops[step][op] = Int('ops_' + str(step) + '_' + op)
+                s.add(Or(ops[step][op] == 0, ops[step][op] == 1))
+            s.add(Sum(ops[step].values()) == 1)
+
+        args = []
+        svs = []
+
+        for step in range(steps):
+            args.append([{}])
+            args[step][0]['zero'] = (zero, Int('args_' + str(step) + '_' + str(0) + '_' + 'zero'))
+            s.add(Or(args[step][0]['zero'][1] == 0, args[step][0]['zero'][1] == 1))
+            args[step][0]['one'] = (one, Int('args_' + str(step) + '_' + str(0) + '_' + 'one'))
+            s.add(Or(args[step][0]['one'][1] == 0, args[step][0]['one'][1] == 1))
+            args[step][0]['m_x'] = (m_x, Int('args_' + str(step) + '_' + str(0) + '_' + 'm_x'))
+            s.add(Or(args[step][0]['m_x'][1] == 0, args[step][0]['m_x'][1] == 1))
+
+            for st in range(step):
+                args[step][0]['svs_' + str(st)] = (svs[st], Int('args_' + str(step) + '_' + str(0) + '_' + 'svs_' + str(st)))
+                s.add(Or(args[step][0]['svs_' + str(st)][1] == 0, args[step][0]['svs_' + str(st)][1] == 1))
+
+            s.add(Sum(map(lambda x: x[1], args[step][0].values())) == 1)
+
+            svs.append(BitVec('svs_' + str(step), 64))
+
+        s.add(m_fx == svs[steps - 1])
+
+    print s.check()
+    print s.model()
+
+solve_model(3, ['id', 'not'], {0: 0})
 
 # a sequence of steps, each step represents a creation of new value out of existing ones
 # initial values are 0, 1 and x_0
